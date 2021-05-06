5f473000ef9d6b39f2d86506f7ae62ce4c1bc533
Ghosterbeef <Ghosterbeef@github.com>
2021-05-06 05:50:19 +0300

    Другой Api геолокации. Добавлен инпут города

--- src/components/HomeComponents/WeatherComponent.vue
+++ src/components/HomeComponents/WeatherComponent.vue
@@ -2,7 +2,7 @@
     <section class="charter weather">
         <h3>Погода</h3>
         <div id="weather_body" class="weather_body">
-            <h4 class="cityName" ref="cityName"></h4>
+            <input type="text" class="cityName" ref="cityName" v-model="city" @change="updateWeather">
             <img class="weatherImg" src="" alt="Текущая погода" ref="weatherImg">
             <p class="description" ref="description"></p>
             <div class="temperature">
@@ -24,17 +24,20 @@
             return {
                 directions: ['Северный', 'Северо-восточный', 'Восточный',
                     'Юго-восточный', 'Южный', 'Юго-западный', 'Западный', 'Северо-западный'],
+                city: ""
             }
         },
         mounted() {
-            fetch("http://www.geoplugin.net/json.gp")
+            fetch("https://api.ipgeolocation.io/ipgeo?apiKey=9a6726603499432885466ac4ffb606b4")
                 .then(response => response.json())
                 .then(json => {
-                    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${json.geoplugin_city}&units=metric&appid=2a0a43cfc4acc7191c01bdc98ed07c9b&lang=ru`)
+                    console.log(json)
+                    this.city=json.geoplugin_city
+                    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${json.city}&units=metric&appid=2a0a43cfc4acc7191c01bdc98ed07c9b&lang=ru`)
                         .then(response => response.json())
                         .then(json => {
                             this.$refs.weatherImg.src = require(`../../assets/icons/WeatherIcons/${json.weather[0].icon}.svg`)
-                            this.$refs.cityName.textContent = json.name
+                            this.city = json.name
                             this.$refs.description.textContent = json.weather[0].description
                             this.$refs.temp.innerHTML = 'Температура ' + json.main.temp + '&deg;'
                             this.$refs.feelsLike.innerHTML = 'Ощущается как ' + json.main.feels_like + '&deg;'
@@ -46,6 +49,24 @@
                         })
                 })
         },
+        methods:{
+            updateWeather: function () {
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=2a0a43cfc4acc7191c01bdc98ed07c9b&lang=ru`)
+                    .then(response => response.json())
+                    .then(json => {
+                        this.$refs.weatherImg.src = require(`../../assets/icons/WeatherIcons/${json.weather[0].icon}.svg`)
+                        //this.$refs.cityName.textContent = json.name
+                        this.$refs.description.textContent = json.weather[0].description
+                        this.$refs.temp.innerHTML = 'Температура ' + json.main.temp + '&deg;'
+                        this.$refs.feelsLike.innerHTML = 'Ощущается как ' + json.main.feels_like + '&deg;'
+                        this.$refs.windSpeed.textContent = 'Скорость ветра ' + json.wind.speed + " м/с"
+                        let degrees = json.wind.deg * 8 / 360
+                        degrees = Math.round(degrees, 0)
+                        degrees = (degrees + 8) % 8
+                        this.$refs.windDir.textContent = "Направление ветра "+this.directions[degrees]
+                    })
+            }
+        }
     }
 </script>
 
@@ -64,7 +85,13 @@
     }
 
     .cityName {
+        background: linear-gradient(to right top, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.3));
+        border: none;
+        border-radius: 5px;
+        color: rgba(44, 62, 80, 0.7);
+        text-align: center;
         font-size: 1.1rem;
+        outline-color: white;
     }
 
     .description:first-letter {
